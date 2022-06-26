43b53d2f9313b38231333e3c8707e25e3f3980aa
uiseok <uiseoks@naver.com>
2022-06-26 23:59:21 +0900

    .

---
+++ Deep Learning/ex19.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+a = np.array([0.3, 2.9, 4.0])
+exp_a = np.exp(a)
+
+print(exp_a)
+
+sum_exp_a = np.sum(exp_a)
+print(sum_exp_a)
+
+y = exp_a / sum_exp_a
+print(y)
+
+# 소프트맥스 함수
+
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+
+    return y

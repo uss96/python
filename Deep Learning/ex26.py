cded206fc3b4ee1d7fe3cebe5fcdc3710cef3c4e
uiseok <uiseoks@naver.com>
2022-07-03 13:07:56 +0900

    .

--- Deep Learning/ex26.py
+++ Deep Learning/ex26.py
@@ -1,1 +1,52 @@
-.
+import numpy as np
+import matplotlib.pylab as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+# 2변수 함수
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+
+# x0 = 3, x1 = 4일 때, x0에 대한 편미분을 구하여라.
+def function_tmp1 (x0):
+    return x0*x0 + 4.0**2.0
+
+def numerical_diff (f, x) :
+    h = 1e-4 # 0.0001
+    return (f(x+h) - f(x-h)) / (2*h)
+
+print(numerical_diff(function_tmp1, 3.0))
+
+# x0 =3, x1 = 4일 때, x1에 대한 편미분을 구하여라
+
+def function_tmp2 (x1):
+    return 3.0**2.0 + x1*x1
+
+print(numerical_diff(function_tmp2, 4.0))
+
+# x0과 x1의 편미분을 동시에 계산하는 함수
+
+def numerical_gradient(f, x):
+    h = 1e-4 #0.0001
+    grad = np.zeros_like(x) # x와 형상이 같은 배열을 생성
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        # f(x+h) 계산
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h) 계산
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val # 값 복원
+
+    return grad
+
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
+print(numerical_gradient(function_2, np.array([0.0, 2.0])))
+print(numerical_gradient(function_2, np.array([3.0, 0.0])))
+
+
---
+++ Deep Learning/ex28.py
@@ -0,0 +1,84 @@
+import numpy as np
+
+def numerical_gradient(f, x):
+    h = 1e-4 #0.0001
+    grad = np.zeros_like(x) # x와 형상이 같은 배열을 생성
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        # f(x+h) 계산
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        # f(x-h) 계산
+        x[idx] = tmp_val - h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val # 값 복원
+
+    return grad
+
+# 경사하강법 구현
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+    return x
+
+# 경사하강법 예제로 해보기
+
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+init_x = np.array([-3.0, 4.0])
+print(gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100))
+
+# 경사하강법 예제 시각화
+
+# coding: utf-8
+import numpy as np
+import matplotlib.pylab as plt
+from ex27 import numerical_gradient
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+    x_history = []
+
+    for i in range(step_num):
+        x_history.append( x.copy() )
+
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x, np.array(x_history)
+
+
+def function_2(x):
+    return x[0]**2 + x[1]**2
+
+init_x = np.array([-3.0, 4.0])
+
+lr = 0.1
+step_num = 20
+x, x_history = gradient_descent(function_2, init_x, lr=lr, step_num=step_num)
+
+plt.plot( [-5, 5], [0,0], '--b')
+plt.plot( [0,0], [-5, 5], '--b')
+plt.plot(x_history[:,0], x_history[:,1], 'o')
+
+plt.xlim(-3.5, 3.5)
+plt.ylim(-4.5, 4.5)
+plt.xlabel("X0")
+plt.ylabel("X1")
+plt.show()
+
+# 경사하강법의 학습률이 너무 큰 예시 : lr = 10.0
+init_x = np.array([-3.0, 4.0])
+print(gradient_descent(function_2, init_x=init_x, lr=10.0, step_num=100))
+
+# 경사하강법의 학습률이 너무 작은 예시 : lr = 1e-10
+print(gradient_descent(function_2, init_x=init_x, lr=1e-10, step_num=100))

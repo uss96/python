970ef53b22eb2bdfd399cd91c4237d9357340c73
uiseok <uiseoks@naver.com>
2022-07-05 00:34:34 +0900

    .

---
+++ Deep Learning/5장/ex32.py
@@ -0,0 +1,97 @@
+# 단순한 계층 구현하기
+
+# 곱셈 계층
+
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+        out = x * y
+        return out
+
+    def backward(self, dout):
+        dx = dout * self.y  # x와 y를 바꾼다.
+        dy = dout * self.x
+        return dx, dy
+
+
+# 예제
+
+apple = 100
+apple_num = 2
+tax = 1.1
+
+# 계층들
+mul_apple_layer = MulLayer()
+mul_tax_layer = MulLayer()
+
+# 순전파
+apple_price = mul_apple_layer.forward(apple, apple_num)
+price = mul_tax_layer.forward(apple_price, tax)
+
+print(price)
+
+# 역전파
+dprice = 1
+dapple_price, dtax = mul_tax_layer.backward(dprice)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)
+
+print(dapple, dapple_num, dtax)
+
+
+# 단순한 계층 구현하기
+
+# 덧셈 계층
+
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
+
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+        return dx, dy
+
+
+# 예제
+
+apple = 100
+apple_num = 2
+orange = 150
+orange_num = 3
+tax = 1.1
+
+# 계층들
+mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_apple_orange_layer = AddLayer()
+mul_tax_layer = MulLayer()
+
+# 순전파
+apple_price = mul_apple_layer.forward(apple, apple_num)
+orange_price = mul_orange_layer.forward(orange, orange_num)
+all_price = add_apple_orange_layer.forward(apple_price, orange_price)
+price = mul_tax_layer.forward(all_price, tax)
+
+print(price)
+
+# backward
+dprice = 1
+dall_price, dtax = mul_tax_layer.backward(dprice)  # (4)
+dapple_price, dorange_price = add_apple_orange_layer.backward(dall_price)  # (3)
+dorange, dorange_num = mul_orange_layer.backward(dorange_price)  # (2)
+dapple, dapple_num = mul_apple_layer.backward(dapple_price)  # (1)
+
+print(dapple_num)
+print(dapple)
+print(dorange_num)
+print(dorange)
+print(dtax)

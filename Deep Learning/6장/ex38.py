a19eaedfdbc9f8fc66fb886210be6d74d0cc8413
uiseok <uiseoks@naver.com>
2022-07-23 07:03:33 +0900

    .

--- Deep Learning/6장/ex38.py
+++ Deep Learning/6장/ex38.py
@@ -1,13 +1,13 @@
-# coding: utf-8
+# coding: utf - 8
 import os
 import sys
 
-sys.path.append(/Users/uiseok/PycharmProjects/python/Deep Learning/common_bigdata/util.py)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
+sys.path.append(os.pardir)  # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import matplotlib.pyplot as plt
 from dataset.mnist import load_mnist
-from common.util import smooth_curve
-from common.multi_layer_net import MultiLayerNet
-from common.optimizer import *
+from common_bigdata.util import smooth_curve
+from common_bigdata.multi_layer_net import MultiLayerNet
+from common_bigdata.optimizer import *
 
 # 0. MNIST 데이터 읽기==========
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True)
---
+++ Deep Learning/6장/경로확인.py
@@ -0,0 +1,7 @@
+import os
+
+os.getcwd()
+
+import sys
+
+sys.path.append(r'C:/Users/uiseok/PycharmProjects/python/Deep Learning/common_bigdata')
